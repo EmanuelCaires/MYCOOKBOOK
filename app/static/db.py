c819a9599395dee71ef0bf02d0de8889a93f31b4
emanuel caires <emanuelcaires1@gmail.com>
2024-01-04 22:47:19 +0000

    create requirements file and procfile

--- app/static/db.py
+++
@@ -1,7 +0,0 @@
-# app/db.py
-from flask_pymongo import PyMongo
-
-mongo = PyMongo()
-
-def get_db():
-    return mongo.db
---
+++ app/static/env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "4({pfi]OS|i9*[AJ)sck#nL[XBe$M]")
+os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_DBNAME", "task_manager")
