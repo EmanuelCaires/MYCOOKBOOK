660c00be5475f1295e12133b21105370221621c7
emanuel caires <emanuelcaires1@gmail.com>
2024-01-05 23:01:57 +0000

    appy.py changed

--- app.py
+++ app.py
@@ -1,30 +1,19 @@
+# app/__init__.py
 import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_pymongo import PyMongo
-from bson import ObjectId
-from datetime import datetime
-from urllib.parse import quote_plus
 
 app = Flask(__name__)
 
-# Encode the username and password for MongoDB URI
-username = "emanuelcaires1"
-password = "emanuelcaires1"
-encoded_username = quote_plus(username)
-encoded_password = quote_plus(password)
-
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get(
-    "MONGO_URI",
-    f"mongodb+srv://{encoded_username}:{encoded_password}@cluster0.wmcpp51.mongodb.net/myCookbookDB?retryWrites=true&w=majority",
-)
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI", "mongodb+srv://emanuelcaires1@admin:emanuelcaires1@cluster0.wmcpp51.mongodb.net/myCookbookDB?retryWrites=true&w=majority")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
 # Create 'users' collection if not exists
 with app.app_context():
-    if 'users' not in mongo.db.list_collection_names():
+    if mongo and 'users' not in mongo.db.list_collection_names():
         users = mongo.db.users
         users.insert_one({'username': 'your_username', 'password': 'your_password'})
 
@@ -50,6 +39,7 @@ def logout():
     session.pop('user', None)
     flash('You have been logged out', 'info')
     return redirect(url_for('index'))
+appy.py changed
 
 
 
