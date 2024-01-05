f8fd1c78880a6066f4f4eba9c8ecfad5a56985a0
emanuel caires <emanuelcaires1@gmail.com>
2024-01-05 00:18:32 +0000

    updated Jinja version

--- app/static/env.py
+++ app/static/env.py
@@ -3,5 +3,6 @@ import os
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "4({pfi]OS|i9*[AJ)sck#nL[XBe$M]")
-os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://emanuelcaires1:emanuelcaires1@cluster0.wmcpp51.mongodb.net/?myCookbookDB
+retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "task_manager")
