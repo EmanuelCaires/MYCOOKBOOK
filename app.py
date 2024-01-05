0382d905aeed4bac9c5dc116f54270073dfa1a3d
emanuel caires <emanuelcaires1@gmail.com>
2024-01-05 00:45:26 +0000

    delete line on app.py

--- app.py
+++ app.py
@@ -18,10 +18,6 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_tasks")
-def get_tasks():
-    tasks = mongo.db.tasks.find()
-    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
