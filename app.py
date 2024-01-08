41ccde628c19e05069aa29664d5c44729fb7e3e7
emanuel caires <emanuelcaires1@gmail.com>
2024-01-08 23:00:43 +0000

    rename templates

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
-import os
-from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
+from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-if os.path.exists("env.py"):
-    import env
-
 
 app = Flask(__name__)
-
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
-
+app.config['MONGO_URI'] = 'mongodb+srv://emanuelcaires1:emanuelcaires1@cluster0.wmcpp51.mongodb.net/?'
 mongo = PyMongo(app)
 
+def login():
+    if request.method == 'POST':
+        # Handle the login process here
+        return render_template('login.html')
 
-@app.route("/")
-@app.route("/get_recipes")
-def get_recipes():
-    tasks = mongo.db.recipes.find()
-    return render_template("tasks.html", recipes=tarecipes)
+# Add your other routes and functions here
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/add_recipe', methods=['GET', 'POST'])
+def add_recipe():
+    if request.method == 'POST':
+        # Add your code to insert a new recipe into the database
+        return redirect(url_for('home'))
+    return render_template('add_recipe.html')
+
+@app.route('/edit_recipe/<recipe_id>', methods=['GET', 'POST'])
+def edit_recipe(recipe_id):
+    if request.method == 'POST':
+        # Add your code to update the recipe in the database
+        return redirect(url_for('home'))
+    # Add your code to retrieve the recipe from the database
+    return render_template('edit_recipe.html')
