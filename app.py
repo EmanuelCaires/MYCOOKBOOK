73a1afa7acb6407042550b296a0822b23a150f6a
emanuel caires <emanuelcaires1@gmail.com>
2024-02-05 00:40:02 +0000

    added templates html

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def get_recipes():
 
 
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -46,6 +47,8 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -59,9 +62,12 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -73,6 +79,26 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
--- templates/static/js/script.js
+++ templates/static/js/script.js
@@ -4,4 +4,20 @@
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
 });
+
+
+
+
+
+/*
+    vanilla JavaScript for MaterializeCSS initialization
+*/
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let sidenavs = document.querySelectorAll(".sidenav");
+//     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+//     let collapsibles = document.querySelectorAll(".collapsible");
+//     let collapsiblesInstance = M.Collapsible.init(collapsibles);
+// });
