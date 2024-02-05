14937f8dc39fc3da7610d1bd93c28e480679eba4
emanuel caires <emanuelcaires1@gmail.com>
2024-02-05 23:10:31 +0000

    added changes to html files

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_recipes")
 def get_recipes():
-    recipes = mongo.db.recipes.find()
+    recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
 
 
