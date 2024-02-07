2cbd9811e9a1fdbf9cd9f3e6f3d3f7ff47990f64
emanuel caires <emanuelcaires1@gmail.com>
2024-02-07 23:55:56 +0000

    added new content

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+
 if os.path.exists("env.py"):
     import env
-
 
 app = Flask(__name__)
 
@@ -17,15 +17,14 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-
 @app.route("/")
-@app.route("/get_recipes")
-def get_recipes():
-    recipes = list(mongo.db.recipes.find())
-    return render_template("recipes.html", recipes=recipes)
+@app.route("/login")
+def home():
+    # Check if user is logged in
+    if "user" in session:
+        session.pop("user")  # Logout the user if logged in
 
-
-
+    return render_template("login.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -50,7 +49,6 @@ def register():
         return redirect(url_for("profile", username=session["user"]))
 
     return render_template("register.html")
-
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -80,28 +78,99 @@ def login():
 
     return render_template("login.html")
 
+from flask import request
+
+# Update your profile route to handle pagination
+from math import ceil
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
-    # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
 
     if session["user"]:
-        return render_template("profile.html", username=username)
+        page = request.args.get("page", default=1, type=int)
+        per_page = 10
+
+        user_recipes = list(mongo.db.recipes.find({"created_by": session["user"]})
+                            .skip((page - 1) * per_page).limit(per_page))
+
+        # Calculate total number of pages
+        total_recipes = mongo.db.recipes.count_documents({"created_by": session["user"]})
+        total_pages = ceil(total_recipes / per_page)
+
+        return render_template("profile.html", username=username, user_recipes=user_recipes, total_pages=total_pages)
 
     return redirect(url_for("login"))
 
+
+# app.py
 
 @app.route("/logout")
 def logout():
     # remove user from session cookie
     flash("You have been logged out")
     session.pop("user")
-    return redirect(url_for("login"))
+    return redirect(url_for("login"))  # Redirect to the login page after logging out
 
+
+@app.route("/add_recipe", methods=["GET", "POST"])
+def add_recipe():
+    if request.method == "POST":
+        recipe = {
+            "recipe_name": request.form.get("recipe_name"),
+            "ingredients": request.form.get("ingredients"),
+            "description": request.form.get("description"),
+            "cooking_instructions": request.form.get("cooking_instructions"),
+            "preparation_time": request.form.get("preparation_time"),
+            "serving_size": request.form.get("serving_size"),
+            "created_by": session["user"]
+        }
+        mongo.db.recipes.insert_one(recipe)
+        flash("Recipe added successfully!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("add_recipe.html")
+
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.delete_one({"_id": ObjectId(recipe_id)})
+    flash("Recipe deleted successfully!")
+    return redirect(url_for("profile", username=session["user"]))
+
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    if request.method == "POST":
+        updated_recipe = {
+            "recipe_name": request.form.get("recipe_name"),
+            "ingredients": request.form.get("ingredients"),
+            "description": request.form.get("description"),
+            "cooking_instructions": request.form.get("cooking_instructions"),
+            "preparation_time": request.form.get("preparation_time"),
+            "serving_size": request.form.get("serving_size"),
+            "created_by": session["user"]
+        }
+        mongo.db.recipes.update_one({"_id": ObjectId(recipe_id)}, {"$set": updated_recipe})
+        flash("Recipe updated successfully!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("edit_recipe.html", recipe=recipe)
+
+
+@app.route("/view_recipe/<recipe_id>")
+def view_recipe(recipe_id):
+    # Retrieve the recipe details from the database
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    if recipe:
+        return render_template("view_recipe.html", recipe=recipe)
+    else:
+        flash("Recipe not found")
+        return redirect(url_for("profile", username=session["user"]))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
+
--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,6 @@
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
-    $(".collapsible").collapsible();
 });
 
 
